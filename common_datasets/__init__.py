86a3d2f932725f084dbadeeb8f3ea34e7d3831a9
György Kovács <gyuriofkovacs@gmail.com>
2022-08-29 14:28:29 +0200

    adding some missing imports

--- common_datasets/__init__.py
+++ common_datasets/__init__.py
@@ -7,3 +7,6 @@ from ._io import references
 from . import binary_classification
 from . import multiclass_classification
 from . import regression
+from . import clustering
+
+from __version__ import *
